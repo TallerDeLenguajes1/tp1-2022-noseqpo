d0659a773a37abbfac9b31e805f507058fbb0179
Daniel Paz <danielnicolaspeace@gmail.com>
2022-08-31 15:32:41 -0300

    punto 2, kinda

---
+++ TP01/TP01/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TP01;
+Console.WriteLine("Hello, World!");
+
+
+PuntoDos.ProblemaDos();
---
+++ TP01/TP01/PuntoDos.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP01
+{
+    internal class PuntoDos
+    {
+        public static void ProblemaUno()
+        {
+            string temp;
+
+            Console.WriteLine("Ingrese un numero entero");
+            temp = Console.ReadLine();
+
+            try
+            {
+                int numero = Int32.Parse(temp);
+
+                Console.WriteLine("El cuadrado de {0} es {1}", numero, Math.Pow(numero, 2));
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+
+        public static void ProblemaDos()
+        {
+            string temp;
+            string temp2;
+
+            Console.WriteLine("Ingrese dividendo:");
+            temp = Console.ReadLine();
+            Console.WriteLine("Ingrese divisor:");
+            temp2 = Console.ReadLine();
+
+            try
+            {
+                int divd = Int32.Parse(temp);
+                int divs = Int32.Parse(temp2);
+
+                Console.WriteLine("La division es: " + divd/divs);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+    }
+}
